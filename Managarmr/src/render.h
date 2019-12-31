4d4ab57ea04880bb34ae8124edf2f879546753dc
drage0 <alek.sandar0@yandex.com>
2019-12-31 13:42:29 +0100

    render_being- add missing `restrict` qualifier.

--- Managarmr/src/render.h
+++ Managarmr/src/render.h
@@ -3,4 +3,4 @@
 #include "being.h"
 
 extern void render_beingtarget(SDL_Renderer *renderer, const struct Being * restrict being);
-extern void render_being(SDL_Renderer *renderer, const struct Being *being, int iscaught);
+extern void render_being(SDL_Renderer *renderer, const struct Being * restrict being, int iscaught);
